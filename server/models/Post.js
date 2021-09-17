d0acddb0188d30dcabc7e84df4f42a33a91ad299
drewmcreynolds <drew.gmcreynolds@gmail.com>
2021-09-17 12:00:07 -0600

    fixed formmating on Post Model server side

--- server/models/Post.js
+++ server/models/Post.js
@@ -2,17 +2,17 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 export const PostSchema = new Schema(
-    {
-        title: { type: String, reqired: true },
-        description: { type: String, required: true },
-        creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true }
-    },
-    { timestamps: true, toJSON: {  virtuals: true } }
+  {
+    title: { type: String, reqired: true },
+    description: { type: String, required: true },
+    creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true }
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
 )
 
 PostSchema.virtual('creator', {
-    localField: 'creatorId', 
-    foreignField: '_id',
-    ref: 'Account',
-    justOne: true
+  localField: 'creatorId',
+  foreignField: '_id',
+  ref: 'Account',
+  justOne: true
 })
